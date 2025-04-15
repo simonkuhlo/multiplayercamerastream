8a1bac7af5b17cf907fae00e8486c34b44ffd380
Simon <siga241@gmail.com>
2025-04-15 08:14:36 +0200

    Initial commit

---
+++ player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody3D
+
+# Movement
+@export var speed = 5.0
+@export var sprint_speed = 8.0
+@export var jump_velocity = 4.5
+@export var mouse_sensitivity = 0.002
+
+# Camera
+@export var camera_pivot:Marker3D
+@export var camera:Camera3D
+
+var gravity = 9.8
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		camera_pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, -1.5, 1.5)
+
+func _physics_process(delta):
+	# Gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Movement
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var current_speed = sprint_speed if Input.is_action_pressed("sprint") else speed
+	
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+
+	move_and_slide()
